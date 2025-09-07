15a55dad8feb0d17aaf8277abcbf3bd36a8a68c6
Juan <juanawbest@gmail.com>
2025-09-07 19:36:01 +0800

    9/7/2025 Saved

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,7 +11,7 @@ class AuthController extends Controller
 {
     public function showLoginForm()
     {
-        return view('auth.login');
+        return view('auth');
     }
 
     public function login(Request $request)
